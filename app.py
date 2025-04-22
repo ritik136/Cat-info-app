3d1010aa327fe143aaf480a6fc461ec9b2436317
ritik136 <ritik.sharma.2305@gmail.com>
2025-04-22 19:23:39 +0530

    Create app.py
    Python file

---
+++ app.py
@@ -0,0 +1,21 @@
+import requests
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def cat_info():
+    url = "https://api.freeapi.app/api/v1/public/cats/cat/random"
+    response = requests.get(url)
+    data = response.json()
+    if data["success"] and "data" in data:
+        sub_data = data["data"]
+        name = sub_data["name"]
+        image = sub_data["image"]
+        origin = sub_data["origin"]
+        return render_template("cat.html", cat_name=name, cat_origin=origin, cat_image_url=image)
+    else:
+        return "Failed to fetch cat data.", 500
+
+if __name__ == "__main__":
+    app.run(debug=True)
